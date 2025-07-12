063b86e92b71f0586f68beaae1ac78b73648f77e
Poonam606 <poonamyadav464@gmail.com>
2025-07-12 11:05:02 +0530

    Initial Commit

---
+++ AtlysTest/AtlysTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlysTestApp.swift
+//  AtlysTest
+//
+//  Created by poonam on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AtlysTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AtlysTestUITests/AtlysTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AtlysTestUITestsLaunchTests.swift
+//  AtlysTestUITests
+//
+//  Created by poonam on 12/07/25.
+//
+
+import XCTest
+
+final class AtlysTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
